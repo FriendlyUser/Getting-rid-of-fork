11f0f94ee602f7e27c9b112cdf742f6293923305
dli <davidli012345@gmail.com>
2019-05-20 16:45:05 -0700

    work damn it work

--- mv_files.sh
+++ mv_files.sh
@@ -10,12 +10,13 @@ find . -type f -name "*.mp4" -print0 | while IFS= read -r -d '' fullfile;
    # filename=$(basename -- "$fullfile")
    extension="${fullfile##*.}"
    filename="${fullfile%.*}"
-   mp3_file="${fullname%.mp4}.mp3"
+   echo $filename
+   mp3_file="${filename}.mp3"
    # "ffmpeg, "-y", "-loglevel", "quiet", "-i", fname, "-vn"  mp3"
    ffmpeg -y -loglevel quiet -i $fullfile -vn $mp3_file
    filename2="${f1##*/}" 
    echo ${filename2}        
    mv "${mp3_file}" cait_music || true
-   mv $fullname cait_music
+   mv "$fullname" cait_music || true
 done
 ls
