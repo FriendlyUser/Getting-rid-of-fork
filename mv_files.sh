6685d6c0944b0b33154fe782b89ab40958b38095
dli <davidli012345@gmail.com>
2019-05-20 16:38:02 -0700

    trying agian

--- mv_files.sh
+++ mv_files.sh
@@ -6,12 +6,13 @@ ls
 echo "Starting to grab files"
 find . -type f -name "*.mp4" -print0 | while IFS= read -r -d '' fullfile;
  do
-   filename=$(basename -- "$fullfile")
-   extension="${filename##*.}"
-   filename="${filename%.*}"
+   echo $fullfile
+   # filename=$(basename -- "$fullfile")
+   extension="${fullfile##*.}"
+   filename="${fullfile%.*}"
    mp3_file="${fullname%.mp4}.mp3"
    # "ffmpeg, "-y", "-loglevel", "quiet", "-i", fname, "-vn"  mp3"
-   # ffmpeg -y -loglevel quiet -i fullfile -vn mp3_file
+   ffmpeg -y -loglevel quiet -i $fullfile -vn $mp3_file
    filename2="${f1##*/}" 
    echo ${filename2}        
    mv "${mp3_file}" cait_music || true
