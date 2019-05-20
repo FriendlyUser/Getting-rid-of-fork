48ae965f67b751ab4f259c5d4e14b517a169ad56
dli <davidli012345@gmail.com>
2019-05-20 15:40:43 -0700

    trying to build again

--- mv_files.sh
+++ mv_files.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash 
 
 # Make this a parameter later
-mkdir -p cait_music
-for f1 in *.mp3
+mkdir cait_music
+echo "Starting to grab files"
+for fullfile in *.mp4
  do
+   filename=$(basename -- "$fullfile")
+   extension="${filename##*.}"
+   filename="${filename%.*}"
+   mp3_file="${filename}.mp3"
+   # "ffmpeg, "-y", "-loglevel", "quiet", "-i", fname, "-vn"  mp3"
+   ffmpeg -y -loglevel quiet -i fullfile -vn mp3_file
    filename2="${f1##*/}" 
    echo ${filename2}        
-   mv "${f1}" cait_music/
+   mv "${mp3_file}" cait_music/
 done
