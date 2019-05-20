b65a16e531272efd3d884259c7cff4195e111841
dli <davidli012345@gmail.com>
2019-05-20 16:00:49 -0700

    trying again

--- mv_files.sh
+++ mv_files.sh
@@ -3,15 +3,16 @@
 # Make this a parameter later
 mkdir cait_music
 echo "Starting to grab files"
-for fullfile in *.mp3
+for fullname in $(find -name "*.mp3")
  do
    filename=$(basename -- "$fullfile")
    extension="${filename##*.}"
    filename="${filename%.*}"
    mp3_file="${filename}.mp3"
    # "ffmpeg, "-y", "-loglevel", "quiet", "-i", fname, "-vn"  mp3"
-   ffmpeg -y -loglevel quiet -i fullfile -vn mp3_file
+   # ffmpeg -y -loglevel quiet -i fullfile -vn mp3_file
    filename2="${f1##*/}" 
    echo ${filename2}        
-   mv "${mp3_file}" cait_music/
+   mv "${mp3_file}" cait_music/ || true
+   mv fullname cait_music
 done
