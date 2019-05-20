64347d735d1e96da5f0672b7a3e53cbb66e2039a
dli <davidli012345@gmail.com>
2019-05-20 16:10:08 -0700

    last attempt

--- mv_files.sh
+++ mv_files.sh
@@ -8,11 +8,11 @@ for fullname in $(find -name "*.mp4")
    filename=$(basename -- "$fullfile")
    extension="${filename##*.}"
    filename="${filename%.*}"
-   mp3_file="${filename}.mp3"
+   mp3_file="${fullname%.mp4}.mp3"
    # "ffmpeg, "-y", "-loglevel", "quiet", "-i", fname, "-vn"  mp3"
    # ffmpeg -y -loglevel quiet -i fullfile -vn mp3_file
    filename2="${f1##*/}" 
    echo ${filename2}        
-   mv "${mp3_file}" cait_music/ || true
-   mv fullname cait_music
+   mv "${mp3_file}" cait_music || true
+   mv $fullname cait_music
 done
