833bcd1094faf3a897394ec8e4133998d9244245
dli <davidli012345@gmail.com>
2019-05-20 16:55:16 -0700

    final attempt

--- mv_files.sh
+++ mv_files.sh
@@ -11,9 +11,10 @@ find . -type f -name "*.mp4" -print0 | while IFS= read -r -d '' fullfile;
    extension="${fullfile##*.}"
    filename="${fullfile%.*}"
    echo $filename
-   mp3_file="${filename}.mp3"
+   mp3_file="${fullfile%.*}.mp3" 
+   echo "$mp3_file"
    # "ffmpeg, "-y", "-loglevel", "quiet", "-i", fname, "-vn"  mp3"
-   ffmpeg -y -loglevel quiet -i "$fullfile" -vn "$mp3_file"
+   ffmpeg -y -loglevel quiet -i "$fullfile" -vn "${fullfile%.*}.mp3" 
    filename2="${f1##*/}" 
    echo ${filename2}        
    mv "${mp3_file}" cait_music || true
